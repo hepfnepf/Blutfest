55187a1bc85a8d953cf38a6ffd5ceb56e73e2043
hepfnepf <nichtmeinname2001@gmail.com>
2021-10-01 14:28:19 +0200

    Enemys now lay on the ground after death. After the time given in time_before_fade they start to fade in time_to_fade seconds. After they are not visible anymore they get freed.

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export (int) var speed = 100
 export (int) var max_health = 100
 
+export (float)var time_before_fade=1#after the enemy is dead it takes time_before_fade seconds until the fading begins
+export (float)var time_to_fade=5#how long the fading takes
 
 onready var health_Node = $Health
 onready var ai = $AI
@@ -10,10 +12,14 @@ onready var team_Node = $Team
 onready var movement = $Movement
 onready var animation_player = $AnimationPlayer
 onready var cooldown_timer:Timer = $AttackCooldown
+onready var until_fading_timer:Timer = $TimeUntilFading
 onready var collision_shape:CollisionShape2D = $CollisionShape2D
+
 onready var player = get_node("/root/Game/Player")
 
 var alive :=true
+var fading :=false
+var alpha:float = 1
 
 func handle_hit(damage:int, type:int = 1):
 	if alive:
@@ -32,7 +38,7 @@ func start_death_animation():
 	animation_player.play("Death")
 
 func die():
-	queue_free()
+	until_fading_timer.start(time_before_fade)
 
 func _ready():
 	movement.speed = speed
@@ -41,9 +47,19 @@ func _ready():
 	movement.initialize(self, animation_player)
 	ai.initialize(movement,team_Node.team,player)
 	ai.cooldown_timer = cooldown_timer
-	
+
+func _process(delta):
+	if fading:
+		alpha -= delta/time_to_fade
+		if alpha <=0:
+			queue_free()
+		modulate = Color(1,1,1,alpha)
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Death":
 		die()
+
+
+func _on_TimeUntilFading_timeout():
+	fading = true
