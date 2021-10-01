fa5c9c9853455c24862a26c161fdec25846070a0
hepfnepf <nichtmeinname2001@gmail.com>
2021-10-01 20:19:07 +0200

    Connected time and score to the death screen. Elapsed time is gotte by a timer which returns after every seconds and increases time by 1.

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,51 +6,31 @@ signal health_changed(new_health)
 signal max_health_changed(new_max_health)
 signal exp_changed(new_exp)
 signal exp_limit_changed(new_exp_limit)
+signal time_changed(new_time)
+signal score_changed(new_score)
 
 export (int) var move_speed = 300
 export (int) var max_health = 100 setget set_max_health
 export (int) var health = max_health setget set_health
 
 var experience:int = 0
-var experience_limit=100
-
-onready var Sprite = $Sprites
-onready var AnimationPlayer = $AnimationPlayer
+var experience_limit:int=100
+var score:int = 0 setget set_score
+var alive:bool = true
+var elapsed_time=0 #get increased by 1 sec every time the time counter returns
+#onready var Sprite:Sprite = $Sprites
+#onready var AnimationPlayer = $AnimationPlayer
 onready var Weapon = $Weapon
-
-func animateWalk(move_vec):
-	#does not work
-	if move_vec != Vector2.ZERO:
-		if (move_vec.x < 0):
-			if(move_vec.y < 0):
-				AnimationPlayer.play("walk_forward_left")
-			elif(move_vec.y > 0):
-				AnimationPlayer.play("walk_backwards_left")
-			else:
-				AnimationPlayer.play("walk_left")
-		elif(move_vec.x > 0):
-			if(move_vec.y < 0): 
-				AnimationPlayer.play("walk_forward_right")
-			elif(move_vec.y > 0):
-				AnimationPlayer.play("walk_backwards_right")
-			else:
-				AnimationPlayer.play("walk_right")
-		else:
-			if (move_vec.y < 0):
-				AnimationPlayer.play("walk_forward")
-			elif (move_vec.y > 0):
-				AnimationPlayer.play("walk_backwards")
-			else:
-				AnimationPlayer.play("none")
-			
-		AnimationPlayer.playback_speed = 2.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _physics_process(delta):
+	if !alive:
+		return
+	
 	var move_vec = Vector2()
 	if Input.is_action_pressed("move_up"):
 		move_vec.y -=1
@@ -73,34 +53,51 @@ func _physics_process(delta):
 
 #Health related, maybe should be outsourced to its own node
 func set_health(new_health:int):
+	if !alive:
+		return
 	health = new_health
 	emit_signal("health_changed",health)
 	if health <= 0:
 		die()
 
 func set_max_health(new_max_health:int):
+	if !alive:
+		return
 	max_health = new_max_health
 	emit_signal("max_health_changed",max_health)
 
+func set_score(new_score):
+	if !alive:
+			return
+	score=new_score
+
 func die():
-	print("dead")
 	emit_signal("dead")
+	alive = false
 
 func take_damage(damage:int):
+	if !alive:
+		return
 	set_health(health - damage)
 
 #Experience Management
 func receive_experience(base_xp:int):
+	if !alive:
+		return
 	experience += base_xp
 	while experience >= experience_limit:
 		level_up()
 
 func level_up():
+	if !alive:
+		return
 	experience -= experience_limit
 	emit_signal("exp_changed",experience)
 	next_exp_limit()
 
 func next_exp_limit():
+	if !alive:
+		return
 	experience_limit = experience_limit*2
 	emit_signal("exp_limit_changed",experience_limit)
 	pass
@@ -108,12 +105,22 @@ func next_exp_limit():
 
 
 func set_weapon(weapon):
+	if !alive:
+		return
 	Weapon.set_weapon(weapon)
 
 func _on_Area2D_area_entered(area):
-	print(area.name)
+	if !alive:
+		return
 	if area.is_in_group("Item"):
 		if area.has_method("pick_up"):
 			area.pick_up(self)
 		else:
 			print("Item without pickup function")
+
+
+func _on_ElapsedTime_timeout():
+	if !alive:
+		return
+	elapsed_time += 1
+	print(elapsed_time) 
--- Scenes/UI/DeathScreen.gd
+++ Scenes/UI/DeathScreen.gd
@@ -1,16 +1,21 @@
 extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var scoreLabel = $MarginContainer/VBoxContainer/CenterContainer2/MarginContainer/GridContainer/Score
+onready var timeLabel = $MarginContainer/VBoxContainer/CenterContainer2/MarginContainer/GridContainer/Time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func set_score(score):
+	scoreLabel.text = str(score)
 
+func set_time(time):
+	var minutes = time / 60
+	var seconds = time % 60
+	var str_time = "%02d:%02d" % [minutes, seconds]
+	timeLabel.text = str_time
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
