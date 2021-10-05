af9a4db99ac09d86a1380befed1d836e295c2f41
hepfnepf <agile123@protonmail.com>
2021-10-05 22:47:19 +0200

    Added save games. Managed via the autoloaded SaveManager script. On death highscore/time gets read from save. If new highscore/best time it gets written into the file.

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -20,6 +20,12 @@ func _on_player_death():
 	death_screen.set_score(player.score)
 	death_screen.set_time(player.elapsed_time)
 	spawner.game_alive = false
+	var save_dict= SaveManager.read_savegame()
+	if save_dict["highscore"] < player.score:
+		save_dict["highscore"] = player.score
+	if save_dict["best_time"] < player.elapsed_time:
+		save_dict["best_time"] = player.elapsed_time
+	SaveManager.save(save_dict)
 	
 func _on_enemy_killed(points):
 	player.set_score(player.score + points)
