2ed00c60ae1f135390d474e01458e4c109fb8d74
hepfnepf <agile123@protonmail.com>
2021-11-20 12:05:03 +0100

    Deleted some unnecessary code like the print in the map super class

--- Map.gd
+++ Map.gd
@@ -4,6 +4,4 @@ class_name Map
 onready var xsize = $Texture.texture.get_width() * $Texture.scale.x
 onready var ysize = $Texture.texture.get_height() * $Texture.scale.y
 
-func _ready():
-	print_debug(xsize)
 
