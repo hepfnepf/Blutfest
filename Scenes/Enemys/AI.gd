981336ecc7afd2a555202c5dcf30e61aac813a09
hepfnepf <nichtmeinname2001@gmail.com>
2021-12-08 17:44:34 +0100

    Changed zombie to not use an area but distance calculation every frame.

--- Scenes/Enemys/AI.gd
+++ Scenes/Enemys/AI.gd
@@ -29,6 +29,7 @@ onready var game = get_node_or_null("/root/Game")
 onready var cooldown_timer:Timer = $AttackCooldown
 onready var attention_radius:CollisionShape2D = $AttentionRadius/CollisionShape2D
 onready var reevaluation:Timer = $TargetReevaluation
+onready var player:Player = null
 
 func initialize(movementNode:Movement, team : int):
 	self.movementNode = movementNode
@@ -36,6 +37,8 @@ func initialize(movementNode:Movement, team : int):
 	movementNode.connect("reached_destination",self,"_on_reached_destination")
 	patrol_destination = create_random_patrol_location()
 	current_radius = rand_range(0,max_start_radius)
+	player = get_parent().player
+	target_player = player
 	reevaluation.wait_time = rand_range(15,45)
 	reevaluation.start()
 	#attention_radius.shape.radius = rand_range(0,max_start_radius)
@@ -55,6 +58,9 @@ func _process(delta):
 			movementNode.set_destination(target_player.global_position)
 	else:#state patrol
 		if attention_radius == null:
+			return
+		if target_player.get_global_position().distance_squared_to(self.get_global_position()) < current_radius*current_radius:
+			state = STATE.HUNT
 			return
 		current_radius += attention_radius_gain*delta
 		movementNode.set_destination(patrol_destination)
