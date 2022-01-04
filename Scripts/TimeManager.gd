28264bc350a135848e320d83cc15f3a36ba4ba51
hepfnepf <nichtmeinname2001@gmail.com>
2022-01-04 15:09:04 +0100

    Improved time manager. Allowed tweening of time for a smoother change.

--- Scripts/TimeManager.gd
+++ Scripts/TimeManager.gd
@@ -10,3 +10,10 @@ func set_time_scale(value:float) -> void:
 	AudioServer.global_rate_scale = 2.0 - value
 	
 	emit_signal("time_scale_changed")
+	
+func set_time_tweened(value:float,fade_time:float) -> void:
+	if !is_instance_valid($Tween):
+		return
+	$Tween.remove_all()
+	$Tween.interpolate_property(TimeManager,"time_scale",null,value,fade_time,0,Tween.EASE_OUT)
+	$Tween.start()
