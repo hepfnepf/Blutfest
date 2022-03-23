95059f7cf53dccd27105b3243c3074290ce70a40
hepfnepf <nichtmeinname2001@gmail.com>
2022-03-23 18:17:06 +0100

    Added in background music.

---
+++ Scenes/BackgroundMusic.gd
@@ -0,0 +1,21 @@
+extends AudioStreamPlayer
+
+export (Array, AudioStream) var tracks
+
+
+var currentIndex:int = 0
+
+func _ready():
+	_on_BackgroundMusic_finished()
+
+
+func _on_BackgroundMusic_finished():
+	if currentIndex+1 >= len(tracks):
+		tracks.shuffle()
+		stream = tracks[0]
+		currentIndex = 0
+		play()
+	else:
+		currentIndex += 1
+		stream = tracks[currentIndex]
+		play()
