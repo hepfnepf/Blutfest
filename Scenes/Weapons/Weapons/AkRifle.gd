df9436666dab714fe6bbedfe7c5abdae05e52a3d
hepfnepf <nichtmeinname2001@gmail.com>
2022-03-24 12:55:24 +0100

    Implemented spread based on movement and balanced weapons

--- Scenes/Weapons/Weapons/AkRifle.gd
+++ Scenes/Weapons/Weapons/AkRifle.gd
@@ -5,12 +5,28 @@ extends Weapon
 # var a = 2
 # var b = "text"
 
+#Change how the spread changes are influenced by the players movement
+export (float) var standing_spread_decrease_multiplyer = 1.0
+export (float) var walking_spread_increase_multiplyer = 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#Differentiates the amount of movement. velocity below will count as standing. above will count as walking
+export (float) var standing_walking_differ = 0.2
 
+func decrease_spread(delta:float):
+	if !is_reloading:
+		if player.velocity  < standing_walking_differ:
+			spread -= spread_dec*delta * standing_spread_decrease_multiplyer
+		else:
+			spread -= spread_dec*delta
+		if spread < base_spread:
+			spread = base_spread
+	emit_signal("spread_changed", spread/base_spread)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func increase_spread():
+	if player.velocity  < standing_walking_differ:
+		spread *= 1+spread_inc
+	else:
+		spread *= 1+spread_inc*walking_spread_increase_multiplyer
+	if spread > max_spread:
+		spread = max_spread
+	emit_signal("spread_changed", spread/base_spread)
