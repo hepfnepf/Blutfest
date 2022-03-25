973a39b5e19a3471a23a0f5338a664f22b8507c7
hepfnepf <nichtmeinname2001@gmail.com>
2022-03-25 19:00:43 +0100

    Created a custom bullet for the short shotgun. Its damage depends on the distance to the enemy.

--- Scenes/Items/EffectNodes/AmmoInfinity.gd
+++ Scenes/Items/EffectNodes/AmmoInfinity.gd
@@ -5,26 +5,28 @@ export (float) var fire_rate_multiplyer = 3.0
 var reload_time_delta = 0
 var fire_rate_delta = 0
 
-var weapon:Weapon=null
+var _weapon:Weapon=null
+
 func add_effect():
-	if player.Weapon.current_weapon is Weapon:
+
+	if player.weapon.current_weapon is Weapon:
 		#Skipping reload
-		weapon = player.Weapon.current_weapon
-		reload_time_delta = -weapon.reload_time
-		weapon.reload_time_delta += reload_time_delta
-		weapon.reload_time = 0
+		_weapon = player.weapon.current_weapon
+		reload_time_delta = -_weapon.reload_time
+		_weapon.reload_time_delta += reload_time_delta
+		_weapon.reload_time = 0
 
 		#Increase fire rate
-		var new_fire_rate = int(weapon.fire_rate*fire_rate_multiplyer)
-		fire_rate_delta = new_fire_rate - weapon.fire_rate
-		weapon.fire_rate_delta += fire_rate_delta
-		weapon.set_fire_rate(new_fire_rate)
+		var new_fire_rate = int(_weapon.fire_rate*fire_rate_multiplyer)
+		fire_rate_delta = new_fire_rate - _weapon.fire_rate
+		_weapon.fire_rate_delta += fire_rate_delta
+		_weapon.set_fire_rate(new_fire_rate)
 
 func remove_effect():
-	if is_instance_valid(weapon):
-		weapon.reload_time_delta -= reload_time_delta
-		weapon.reload_time = weapon.reload_time - reload_time_delta
+	if is_instance_valid(_weapon):
+		_weapon.reload_time_delta -= reload_time_delta
+		_weapon.reload_time = _weapon.reload_time - reload_time_delta
 
-		weapon.fire_rate_delta -= fire_rate_delta
-		weapon.set_fire_rate(weapon.fire_rate - fire_rate_delta)
+		_weapon.fire_rate_delta -= fire_rate_delta
+		_weapon.set_fire_rate(_weapon.fire_rate - fire_rate_delta)
 
