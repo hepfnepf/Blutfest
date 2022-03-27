ef93c94e9e16b6815e139b076fc87bf3dd2c9072
hepfnepf <nichtmeinname2001@gmail.com>
2022-03-27 17:27:31 +0200

    Refactored the ammo infinity effect. The stacking of multiple ammo infinities should work now.

--- Scenes/Weapons/Weapons/Weapon.gd
+++ Scenes/Weapons/Weapons/Weapon.gd
@@ -3,16 +3,8 @@ extends Node2D
 signal weapon_switch(new_weapon)
 
 
-#export (PackedScene) var weapon_pistol
+onready var current_weapon = null setget set_weapon
 
-#onready var weapon_pistol = $Pistol
-
-onready var current_weapon = get_child(0) setget set_weapon
-
-#var weapon_dict = {"NONE":weapon_none}
-
-#func _ready():
-#	set_weapon(weapon_pistol)
 
 func set_weapon(weapon):
 	if current_weapon != null:
