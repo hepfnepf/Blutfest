89e73e070bdca9b08d8b95996ac8eb174a013d62
hepfnepf <nichtmeinname2001@gmail.com>
2022-04-08 16:50:58 +0200

    Implmented buttons.

--- Scenes/UI/PauseMenue/CustomSlider/AudioSlider.gd
+++ Scenes/UI/PauseMenue/CustomSlider/AudioSlider.gd
@@ -7,7 +7,7 @@ Allows you to select a channel and maipulate its volume with the slider.
 
 export (String) var audio_channel = "Master"
 
-
+export (Texture) var button_texture_disabled = null
 
 func _ready() -> void:
 	._ready()
@@ -17,3 +17,13 @@ func _on_VSlider_value_changed(value):
 	var sfx_index= AudioServer.get_bus_index(audio_channel)
 	var db_value = linear2db(value)
 	AudioServer.set_bus_volume_db(sfx_index, db_value)
+	button.pressed = false
+
+func _on_Button_toggled(button_pressed):
+	var sfx_index= AudioServer.get_bus_index(audio_channel)
+	if button_pressed:
+		AudioServer.set_bus_volume_db(sfx_index, linear2db(0))
+		button.icon = button_texture_disabled
+	else:
+		AudioServer.set_bus_volume_db(sfx_index, linear2db(slider.value))
+		button.icon = button_texture
