91ef22b06c793f018c6c9d848e914076452cc3c0
hepfnepf <nichtmeinname2001@gmail.com>
2022-04-10 20:12:03 +0200

    Implemented music controll logic.

---
+++ Scenes/UI/PauseMenue/MusicController.gd
@@ -0,0 +1,34 @@
+extends HBoxContainer
+
+onready var info_label = $TrackInfoLabel
+
+var music_player:BackgroundMusic = null#is gotten from globals after it was set there
+
+func _ready()->void:
+	Globals.connect("music_player_set",self,"set_music_player")#Tells us that the music player was set in globals
+
+func set_music_player()->void:
+	if is_instance_valid(Globals.music_player):
+		music_player = Globals.music_player
+		music_player.connect("track_changed",self,"_on_track_changed")
+
+func set_info_label(title:String,artist:String)->void:
+	var text = "%s - %s" % [title,artist]
+	info_label.text = text
+
+func _on_SkipButton_pressed()->void:
+	music_player.skip_track()
+
+
+func _on_LoopButton_toggled(button_pressed)->void:
+	music_player.loop = !music_player.loop
+
+func _on_track_changed(path_to_track:String)->void:
+	"""
+	For this to work properly, the files have to be named accordingly: Ttile_Artist_EverythingElse
+	"""
+	var file_name = path_to_track.split("/")[-1]
+	var track_splited = file_name.split("_")
+	if track_splited.size() >= 2:
+		set_info_label(track_splited[0],track_splited[1])
+
