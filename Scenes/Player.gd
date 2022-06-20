0ff294037005effa3a742fb3e0e3564a2bf9257b
hepfnepf <nichtmeinname2001@gmail.com>
2022-06-20 20:03:05 +0200

    Visual indicator now works.

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -8,6 +8,7 @@ signal exp_changed(new_exp)
 signal exp_limit_changed(new_exp_limit)
 signal time_changed(new_time)
 signal score_changed(new_score)
+signal lock_changed(new_lock_state)
 
 export (int) var move_speed = 300
 export (int) var max_health = 100 setget set_max_health
@@ -44,8 +45,9 @@ func _physics_process(delta):
 		return
 
 	#Check if the lock-state has to be changed
-	if Input.is_action_pressed("lock_weapon"):
+	if Input.is_action_just_pressed("lock_weapon"):
 		locked = !locked
+		emit_signal("lock_changed",locked)
 
 	#Movement
 	var move_vec = Vector2()
@@ -70,8 +72,6 @@ func _physics_process(delta):
 
 	#self.look_at(get_global_mouse_position()-self.position)
 	look_at(get_global_mouse_position())
-
-
 
 #Health related, maybe should be outsourced to its own node
 func set_health(new_health:int):
