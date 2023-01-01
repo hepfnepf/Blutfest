96b19ea0c5ea95b811c75835511e820af57e02d7
hepfnepf <agile123@protonmail.com>
2023-01-01 15:55:32 +0100

    Added in bazooka. Still needs balancing and sounds.

---
+++ Scenes/Weapons/Weapons/Bazooka.gd
@@ -0,0 +1,17 @@
+extends Weapon
+
+
+export var explosion_size:float = 1.0
+
+
+func shoot_bullet():
+	var bullet = BulletPool.get_bullet(Bullet,bullet_spawn_position.global_position)
+	var dir_vector = bullet_spawn_position.global_position.direction_to(bullet_spawn_direction.global_position).normalized()
+	var rot = dir_vector.angle()+ rng.randf_range(-spread,spread)
+	bullet.rotation = rot
+	bullet.direction = Vector2.RIGHT.rotated(rot)
+	bullet.speed = speed
+	bullet.p_range = max_range
+	bullet.damage = damage
+	bullet.exp_size = explosion_size
+	increase_spread()
