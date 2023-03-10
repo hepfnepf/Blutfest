1e45d0fb5889f6d34870bad211f8e91f93feed2e
hepfnepf <nichtmeinname2001@gmail.com>
2023-03-10 23:56:54 +0100

    Fixed problems with the animation: The animation would not work in the actual game, because it used the process delta for pacing. Since the game gets paused when you have to select a perk, the delta was always 0. Now it uses the system time during the paused game to circumvent this problem.

--- Scenes/Perks/PerkCard.gd
+++ Scenes/Perks/PerkCard.gd
@@ -12,27 +12,33 @@ onready var rarityLabel:Label=$"%RarityLabel"
 onready var icon:TextureRect=$"%Icon"
 onready var descLabel:Label=$"%DescriptionLabel"
 
-export (float) var hover_size = 1.0
-export (float) var up_duration = 2 #
-export (float) var down_duration = 2 #
+export (float) var hover_size = 1.3
+export (float) var up_duration = 2
+export (float) var down_duration = 2
 var min_size:Vector2 = Vector2(0,0)
 
 var is_hovered:bool = false
 var weight:float = 0.0
+var last_time:float = 0 #needed to make my own delta for the animation, since the delta in process does not work if the game is paused
 
 func _ready()->void:
 	display_perk()
 	min_size = rect_min_size
+	last_time = OS.get_ticks_msec()
 
 func _process(delta: float) -> void:
+	#makeing my own delta for the animation, since the delta in process does not work if the game is paused
+	var new_time = OS.get_ticks_msec()
+	delta = (new_time-last_time)/100
+
 	if is_hovered:
-		print(weight)
-		weight += delta*10/up_duration
+		weight += delta/up_duration
 	else:
-		weight -= delta*10/down_duration
+		weight -= delta/down_duration
 
 	weight=clamp(weight,0,1)
 	rect_min_size=min_size.linear_interpolate(min_size*Vector2(hover_size,hover_size), weight)
+	last_time = new_time
 
 func display_perk()->void:
 	if perk == null:
