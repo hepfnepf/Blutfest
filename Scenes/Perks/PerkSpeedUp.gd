cd93e8632e578ef24aea17e3c892cff1abeb2afe
hepfnepf <nichtmeinname2001@gmail.com>
2023-04-15 22:03:55 +0200

    Switched speed to perk to use percentages.

--- Scenes/Perks/PerkSpeedUp.gd
+++ Scenes/Perks/PerkSpeedUp.gd
@@ -1,9 +1,9 @@
 extends Perk
 
-export(float)var speed_multi=1.2
+export(float)var speed_inc_perc=20
 
 func add_effect()->void:
-	player.move_speed *=speed_multi
+	player.move_speed *=1+speed_inc_perc/100
 
 func get_desc():
-	return tr(desc)%((speed_multi-1.0)*100.0)
+	return tr(desc)%(speed_inc_perc)
