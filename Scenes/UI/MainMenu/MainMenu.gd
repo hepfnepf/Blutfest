c8d1a00aa165f33374918885e11d3e6294a4958c
hepfnepf <nichtmeinname2001@gmail.com>
2023-08-02 21:05:32 +0200

    Added music to the main menu. Added main menu as starting scene.

--- Scenes/UI/MainMenu/MainMenu.gd
+++ Scenes/UI/MainMenu/MainMenu.gd
@@ -4,6 +4,21 @@ extends CanvasLayer
 onready var creditsScreen = $CreditsScreen
 onready var settingsScreen = $SettingsScreen
 
+func _ready() -> void:
+	#Retrieve volume settings
+	var sg = SaveManager.read_saveOptions()
+
+	var sfx_db_value = linear2db(sg["sfx_volume"])
+	var music_db_value = linear2db(sg["music_volume"])
+	print_debug(music_db_value)
+
+	var sfx_index= AudioServer.get_bus_index("SFX")
+	var music_index= AudioServer.get_bus_index("Music")
+
+	AudioServer.set_bus_volume_db(sfx_index, sfx_db_value)
+	AudioServer.set_bus_volume_db(music_index, music_db_value)
+
+
 func _on_PlayButton_pressed() -> void:
 	get_tree().change_scene("res://Scenes/Game.tscn")
 
@@ -16,3 +31,7 @@ func _on_OptionsButton_pressed() -> void:
 func _on_CreditsButton_pressed() -> void:
 	creditsScreen.visible = true
 	creditsScreen.hide_background_color()
+
+
+func _on_ExitButton_pressed() -> void:
+	get_tree().quit()
