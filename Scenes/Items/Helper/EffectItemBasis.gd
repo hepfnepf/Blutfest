71e4324bd36f7aca4994e13b8bf43ac2e451b809
hepfnepf <nichtmeinname2001@gmail.com>
2024-12-07 14:22:50 +0100

    Fixed naming issues.

--- Scenes/Items/Helper/EffectItemBasis.gd
+++ Scenes/Items/Helper/EffectItemBasis.gd
@@ -8,6 +8,5 @@ func pick_up(player:Player):
 	if effect_node != null:
 		var _effect = effect_node.instance()
 		player.add_child(_effect)
-	player.stats_add_powerup(get_script().get_path())
-	explode_if_enabled(player)
-	queue_free()
+
+	.pick_up(player)
--- Scenes/Items/Helper/ExplosionItemBasis.gd
+++ Scenes/Items/Helper/ExplosionItemBasis.gd
@@ -4,14 +4,15 @@ export (float) var damage = 100.0
 export (float) var exp_size = 5.0
 export (PackedScene) var explosion
 
-func pick_up(_player:Player):
+func pick_up(player:Player):
 	var _exp:Node2D = explosion.instance()
 	_exp.damage = damage
-	_exp.scale.x = exp_size*_player.explosion_amt_size_multi
+	_exp.scale.x = exp_size*player.explosion_amt_size_multi
 	_exp.scale.y = _exp.scale.x
 	_exp.global_position = global_position
 	call_deferred("_add_to_game",_exp)
-	_player.explosion_amnt+=1
+	player.explosion_amnt+=1
+	player.stats_add_powerup(item_name)
 	queue_free()
 
 func _add_to_game(_exp):
