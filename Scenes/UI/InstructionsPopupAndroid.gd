ac447b4ec3cc64569db73c0c7660e64cd8e7253c
hepfnepf <69308038+hepfnepf@users.noreply.github.com>
2025-09-05 17:35:57 +0200

    Changed the instruction popup to fit android.

---
+++ Scenes/UI/InstructionsPopupAndroid.gd
@@ -0,0 +1,20 @@
+extends Popup
+
+onready var androidWarningLabel = $CenterContainer/PanelContainer/VBoxContainer/AndroidWarning
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("help") and visible:
+		hide()
+
+	elif Input.is_action_just_pressed("help") and !visible and !get_tree().paused:#game should not be in main menu or card selection
+		popup()
+
+func _on_InstructionsPopup_about_to_show() -> void:
+	Globals.cursor_manager.set_cursor(Cursor.CURSOR_TYPE.DEFAULT)
+	get_tree().paused = true
+
+
+func _on_InstructionsPopup_popup_hide() -> void:
+	get_tree().paused = false
+	Globals.cursor_manager.set_cursor(Cursor.CURSOR_TYPE.CROSSHAIR)
+	Globals.first_start=false
