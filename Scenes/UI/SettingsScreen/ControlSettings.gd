f53c48679dbe1d7cab7e74beda41aa4a36563e9b
hepfnepf <nichtmeinname2001@gmail.com>
2026-03-13 12:29:05 +0100

    Replaced individual buttons and labels with new rebind row element.

--- Scenes/UI/SettingsScreen/ControlSettings.gd
+++ Scenes/UI/SettingsScreen/ControlSettings.gd
@@ -1,17 +1,20 @@
 extends "res://Scenes/UI/SettingsScreen/ExtendedTabs.gd"
 
 onready var zoom_inverted_toggle_button:CheckBox = $"%CheckBox"
-onready var button_container:GridContainer = $MarginContainer/VBoxContainer/GridContainer
+onready var button_container:VBoxContainer = $MarginContainer/VBoxContainer/GridContainer
 onready var mode_selector = $MarginContainer/VBoxContainer/HModeSelector
 
 func _ready():
 	if Globals.android:
 		queue_free()
 	mode_selector.connect("modeSwitched",self,"_on_HModeSelector_modeSwitched")
+	set_mode(mode_selector.get_mode())
+
+func set_mode(mode:String)->void:
+	for child in button_container.get_children():
+		if child.has_method("set_mode_by_string"):
+			child.set_mode_by_string(mode)
 	
 
 func _on_HModeSelector_modeSwitched(mode:String):
-	for child in button_container.get_children():
-		print_debug(child.get_class())
-		if child.has_method("set_mode_by_string"):
-			child.set_mode_by_string(mode)
+	set_mode(mode)
